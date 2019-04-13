e0115c1f793c775637d41b976681f5a1fcd010f3
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2019-04-13 23:34:51 +0200

    Wrapped components inside the game component, added routing

--- hangman/src/app/app-routing.module.ts
+++ hangman/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: GameComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- hangman/src/app/app.module.ts
+++ hangman/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { NavComponent } from './nav/nav.component';
+import { GameComponent } from './game/game.component';
+import { KeyboardComponent } from './game/keyboard/keyboard.component';
+import { PictureComponent } from './game/picture/picture.component';
+import { KeywordComponent } from './game/keyword/keyword.component';
+import { ContactComponent } from './contact/contact.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    NavComponent,
+    GameComponent,
+    KeyboardComponent,
+    PictureComponent,
+    KeywordComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
